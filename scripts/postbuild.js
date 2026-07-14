cdbcbb04c9428788559eba88f8e5ee9082ad58e6
FinancialFlowHub Deployer <deployer@financialflowhub.internal>
2026-07-15 03:48:56 +0530

    fix: copy sitemap-0.xml (actual URLs) to sitemap.xml instead of the empty index wrapper

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -2,12 +2,13 @@ import fs from 'fs';
 import path from 'path';
 
 const distDir = path.join(process.cwd(), 'dist');
-const sitemapIndex = path.join(distDir, 'sitemap-index.xml');
+const sitemapSource = path.join(distDir, 'sitemap-0.xml');
 const sitemapTarget = path.join(distDir, 'sitemap.xml');
 
-if (fs.existsSync(sitemapIndex)) {
-  fs.copyFileSync(sitemapIndex, sitemapTarget);
-  console.log('Successfully duplicated sitemap-index.xml to sitemap.xml');
+if (fs.existsSync(sitemapSource)) {
+  fs.copyFileSync(sitemapSource, sitemapTarget);
+  console.log('Successfully copied sitemap-0.xml to sitemap.xml');
 } else {
-  console.error('sitemap-index.xml not found in dist/');
+  console.error('sitemap-0.xml not found in dist/');
 }
+
